5d71d40b47d3bd2e94cc776171a2ebe434551a10
Vladyslav <v2rockua@gmail.com>
2023-07-24 18:23:23 +0700

    feat: add account page & connect component

---
+++ app/account/components/connect.tsx
@@ -0,0 +1,21 @@
+'use client';
+import React from 'react';
+import { useConnect } from 'wagmi';
+import { InjectedConnector } from 'wagmi/connectors/injected';
+
+export function Connect() {
+  const { connect } = useConnect({
+    connector: new InjectedConnector()
+  });
+
+  const connectMetamask = () => connect();
+
+  return (
+    <div className='relative flex place-items-center flex-col max-w-4xl text-center w-full bg-fuchsia-300/10 p-10 rounded-lg'>
+      <h2 className='text-4xl mb-10'>Please connect wallet</h2>
+      <button className='btn btn-secondary btn-lg max-w-sm w-full' onClick={connectMetamask}>
+        connect
+      </button>
+    </div>
+  );
+}
---
+++ app/account/page.tsx
@@ -0,0 +1,9 @@
+import { Connect } from './components';
+
+export default function Account() {
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-center p-24'>
+      <Connect />
+    </div>
+  );
+}
