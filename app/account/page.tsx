18c315aa50ccb9116e5ee860ade91af37a80f820
Vladyslav <v2rockua@gmail.com>
2023-07-25 01:18:14 +0700

    feat: add page title

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,14 +1,15 @@
 'use client';
 
-import { useAccount, useConnect } from 'wagmi';
-import { Connect, TokenList } from './components';
+import { useAccount } from 'wagmi';
+import { Connect, WalletInfo } from './components';
 
 export default function Account() {
-  const { address, isConnected } = useAccount();
+  const { isConnected } = useAccount();
 
   return (
     <div className='flex min-h-screen flex-col items-center justify-center p-24'>
-      {isConnected ? <>address: {address}</> : <Connect />}
+      <h1 className='text-6xl mb-12'>Account details</h1>
+      {isConnected ? <WalletInfo /> : <Connect />}
     </div>
   );
 }
