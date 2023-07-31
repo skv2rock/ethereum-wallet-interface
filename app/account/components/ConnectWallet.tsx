0a6e6aca4cf48764226615a9aeba3d0c36d79186
Vladyslav <v2rockua@gmail.com>
2023-07-31 13:10:41 +0700

    bug-fix: prod auto connect wallet

--- app/account/components/ConnectWallet.tsx
+++ app/account/components/ConnectWallet.tsx
@@ -1,14 +1,26 @@
 'use client';
 
-import { useConnect } from 'wagmi';
+import { useEffect } from 'react';
+import { useAccount, useConnect } from 'wagmi';
 import { InjectedConnector } from 'wagmi/connectors/injected';
 
 export default function ConnectWallet() {
+  const { isConnected } = useAccount();
   const { connect, isLoading } = useConnect({
     connector: new InjectedConnector()
   });
 
   const connectMetamask = () => connect();
+
+  useEffect(() => {
+    const state = localStorage.getItem('walletConnectState') || '';
+
+    if (!isConnected && state === 'true') connect();
+  }, [isConnected, connect]);
+
+  useEffect(() => {
+    localStorage.setItem('walletConnectState', isConnected.toString());
+  }, [isConnected]);
 
   return (
     <div className='relative flex place-items-center flex-col max-w-4xl text-center w-full bg-fuchsia-300/10 p-10 rounded-lg'>
